ae4af890618762d39d5820bdbb71d41f3061c7e6
saqlain123 <ssaqlain10@yahoo.com>
2015-10-22 10:59:46 +0500

    dutddrytkfuhf

---
+++ binary tree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+struct node;
+{
+	int data;
+	node *left;
+	node *right;
+	public:
+		int a;
+		cout<<"how much long u wana make :";
+		cin>>a;
+		for(int i=0;i<a;i++)
+		cout<<"enter data";
+		cin>>data;
+		
+		
+}
+}
